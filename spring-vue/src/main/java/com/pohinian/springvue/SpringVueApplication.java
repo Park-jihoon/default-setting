2c6623f11a18736a29ea16d0c5aa94f922a521ff
Park-Jihoon <pohinian@gmail.com>
2018-04-16 13:10:16 +0900

    Hello Worldgit add .

--- spring-vue/src/main/java/com/pohinian/springvue/SpringVueApplication.java
+++ spring-vue/src/main/java/com/pohinian/springvue/SpringVueApplication.java
@@ -1,12 +1,24 @@
 package com.pohinian.springvue;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class SpringVueApplication {
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!!!";
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(SpringVueApplication.class, args);
     }
 }
